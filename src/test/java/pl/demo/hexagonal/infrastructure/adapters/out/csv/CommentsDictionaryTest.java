6135165e994bd8dfe0da8c6fa99454e25c847fc0
0i99 <0i99@pm.me>
2024-01-17 11:20:16 +0100

    support for csv (with external refs) dictionaries

--- src/test/java/pl/demo/hexagonal/infrastructure/adapters/out/csv/CommentsDictionaryTest.java
+++ src/test/java/pl/demo/hexagonal/infrastructure/adapters/out/csv/CommentsDictionaryTest.java
@@ -1,6 +1,5 @@
 package pl.demo.hexagonal.infrastructure.adapters.out.csv;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.assertj.core.api.SoftAssertions;
@@ -10,14 +9,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.core.io.ClassPathResource;
 import pl.demo.hexagonal.infrastucture.adapters.out.csv.CommentsCommentsDictionary;
-import pl.demo.hexagonal.infrastucture.adapters.out.csv.reader.CsvDictionaryLoader;
 import pl.demo.hexagonal.infrastucture.adapters.out.csv.model.Comment;
 import pl.demo.hexagonal.infrastucture.adapters.out.csv.model.Details;
+import pl.demo.hexagonal.infrastucture.adapters.out.csv.reader.CsvDictionaryLoader;
+import pl.demo.hexagonal.infrastucture.adapters.out.csv.reader.DictionaryTemplateLoader;
+import pl.demo.hexagonal.infrastucture.adapters.out.csv.reader.JsonDictionaryTemplateReader;
+import pl.demo.hexagonal.infrastucture.adapters.out.csv.reader.XmlDictionaryTemplateReader;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,6 +24,12 @@ class CommentsDictionaryTest {
 
     @Spy
     CsvDictionaryLoader csvDictionaryLoader = new CsvDictionaryLoader();
+
+    @Spy
+    DictionaryTemplateLoader dictionaryTemplateLoader = new DictionaryTemplateLoader(
+            new JsonDictionaryTemplateReader(new ObjectMapper()),
+            new XmlDictionaryTemplateReader(new XmlMapper())
+    );
 
     @InjectMocks
     CommentsCommentsDictionary commentsDictionary;
@@ -39,35 +44,17 @@ class CommentsDictionaryTest {
         assertions.assertThat(comments).isNotEmpty();
 
         Comment json = comments.get(0);
-        Comment xml = comments.get(1);
 
         assertions.assertThat(json).isNotNull();
         assertions.assertThat(json.getId()).isEqualTo(1L);
         assertions.assertThat(json.getTitle()).isEqualTo("title1");
-        assertions.assertThat(json.getPathToDetails()).isEqualTo("1.json");
+        assertions.assertThat(json.getPathToDetails()).isEqualTo("dict/details/1.json");
 
         assertions.assertThat(json.getDate()).isNotNull();
 
-        Details details = Details.builder().something("sth").data("lorem ipsum").build();
-        ObjectMapper objectMapper = new ObjectMapper();
-        XmlMapper xmlMapper = new XmlMapper();
-        try {
-            System.out.println(objectMapper.writeValueAsString(details));
-            System.out.println(xmlMapper.writeValueAsString(details));
-            File file = new ClassPathResource(xml.getPathToDetails()).getFile();
-            Details details1 = xmlMapper.readValue(file, Details.class);
-            System.out.println(details1);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
-//
-//        File file = new ClassPathResource(json.getDetails()).getFile();
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        MappingIterator<T> readValues = mapper.reader(type).with(bootstrapSchema).readValues(file);
+        assertions.assertThat(json.getDetails()).isEqualTo(
+                Details.builder().something("sth").data("lorem ipsum").build()
+        );
 
         assertions.assertAll();
 
